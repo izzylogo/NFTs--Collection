b4d6e2882d4cf61a67062ebe7978076060af3846
Israel Omitiran <omitiranisrael@gmail.com>
2022-05-07 04:17:02 +0100

    adjusting

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x172A0f8808DA405829BE097aa2295180Ece558E6";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
